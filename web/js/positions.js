91bb194837765317bbff7c45fde02f2d503608bf
caefer <caefer@ical.ly>
2010-11-15 19:23:15 +0100

    * inbetween version

--- web/js/positions.js
+++ web/js/positions.js
@@ -1,84 +1,92 @@
-gjCompositionCanvas = function(form)
-{
-  this.form = form;
+// tryout: melanie mitchel - complexity a guided tour
 
-  /**
-   * updates all positions of embedded design and content elements
-   */
-  this.updatePositions = function(event)
+$.composition = {
+  sortableOptions:{
+    axis:'y', 
+    placeholder:'ui-state-highlight',
+    forcePlaceholderSize:true,
+    over:function(event, ui){rm=false},
+    out:function(event, ui){rm=true},
+    beforeStop:function(event, ui){if(true==rm) ui.item.remove()}
+  },
+  update:function(field, context, id, pos)
   {
-    form = $(event.currentTarget);
-    form.find('.content > .positions_container > li').each(function(i, designelement)
-    {
-      $(designelement).find('input, select, textarea').each(function(j, field)
-      {
-        // update name and id
-        field.name = field.name.replace(/\[design_elements\]\[(?:\d|x)\]/, '[design_elements]['+i+']')
-        field.id = field.id.replace(/design_elements_(?:\d|x)_/, 'design_elements_'+i+'_')
-        // update position
-        if(-1 != field.name.search(/\[design_elements\]\[(?:\d|x)\]\[position\]/))
-        {
-          field.value = i;
-        }
-      });
-      total_content_elements = $(designelement).find('.positions_container > li').length;
-      $(designelement).find('.positions_container > li').each(function(j, contentelement)
-      {
-        $(contentelement).find('input, select, textarea').each(function(k, field)
-        {
-          // update name and id
-          field.name = field.name.replace(/\[contents\]\[x]/, '[contents]['+total_content_elements+']');
-          field.id = field.id.replace(/contents_x/, 'contents_'+total_content_elements+'_');
-          // update position
-          if(-1 != field.name.search(/\[contents\]\[\d\]\[position\]/))
-          {
-            field.value = j;
-          }
-          //console.log(field.name+' => '+field.value);
-        });
-        total_content_elements++;
-      });
-    });
-    //form.unbind();
-    //return false;
-  };
+    id_key  = context == 'contents' ? 'x' : '(?:\d|x)';
+    pos_key = context == 'contents' ? '\d' : '(?:\d|x)';
 
-  this._sortableDefaultOptions = {
-    axis:'y',
-    placeholder: 'ui-state-highlight',
-    forcePlaceholderSize: true,
-    over: function(event, ui){removeMe = 0},
-    out: function(event, ui){removeMe = 1},
-    beforeStop: function(event, ui){if(1 == removeMe) ui.item.remove()}
-  };
-
-  this.receiveDesignElement = function(event, ui)
-  {
-    $(event.target).find('li:.ui-draggable').removeClass('ui-draggable').children('div').toggle();
-    canvas.turnDesignElementsToSortables();
+    field.name  = field.name.replace(new RegExp('/\['+context+'\]\['+id_key+'\]/'), '['+context+']['+id+']');
+    field.id    = field.id.replace(new RegExp('/'+context+'_'+id_key+'_/'), context+'_'+id+'_');
+    field.value = (-1 != field.name.search(new RegExp('/\['+context+'\]\['+pos_key+'\]\[position\]/'))) ? position : field.value;
   }
-
-  this.turnCanvasToSortables = function()
-  {
-    design_element_container = this.form.find('.content > .positions_container');
-    design_element_container.sortable($.extend({receive: this.receiveDesignElement}, this._sortableDefaultOptions)).effect('highlight');
-
-    $('#design_element_source_list > li').draggable({connectToSortable: design_element_container, helper: 'clone'})
-  }
-
-  this.turnDesignElementsToSortables = function()
-  {
-    content_element_containers = this.form.find('.content .design-element-form .positions_container');
-    content_element_containers.sortable(this._sortableDefaultOptions).effect('highlight');
-
-    $('#content_element_source_list .contents > li').draggable({connectToSortable: content_element_containers, helper: 'clone'})
-  }
-
-
-  // initialisation
-  this.form.submit(this.updatePositions);
-  this.turnCanvasToSortables();
-  this.turnDesignElementsToSortables();
 }
 
-jQuery(document).ready(function(){canvas = new gjCompositionCanvas($('.sf_admin_form > form'))});
+$.fn.canvas = function()
+{
+  return this.each(function(){
+    this.designcontainers = function(){return $(this).find('.content > .positions_container')};
+    $(this).submit(function(event){this.designcontainers().each(function(){this.update()});return false;});
+    this.designcontainers().designcontainer();
+  });
+}
+
+$.fn.designcontainer = function()
+{
+  return this.each(function(i){
+    this.contentcontainers = function(){return $(this).find('.positions_container')};
+    this.receive = function(event, ui){this.contentcontainers().contentcontainer()};
+    this.update = function()
+    {
+      num = this.num;
+      $(this).find('input, select, textarea').each(function(j, field)
+      {
+        field.name = field.name.replace(/\[design_elements\]\[(?:\d|x)\]/, '[design_elements]['+i+']');
+        field.id = field.id.replace(/design_elements_(?:\d|x)_/, 'design_elements_'+i+'_');
+        field.value = (-1 != field.name.search(/\[design_elements\]\[(?:\d|x)\]\[position\]/)) ? i : field.value;
+        console.log(field.name+' - '+field.id);
+      });
+      num = this.contentcontainers().length;
+      this.contentcontainers().each(function(){this.update(num)});
+    }
+
+    $(this).sortable($.extend({receive:this.receive}, $.composition.sortableOptions));
+    this.contentcontainers().contentcontainer();
+  });
+}
+
+$.fn.contentcontainer = function(designelements)
+{
+  return this.each(function(i){
+    this.receive = function(event, ui){};
+    this.update = function(total)
+    {
+      $(this).find('input, select, textarea').each(function(j, field, total)
+      {
+        field.name = field.name.replace(/\[contents\]\[x]/, '[contents]['+total+']');
+        field.id = field.id.replace(/contents_x/, 'contents_'+total+'_');
+        field.value = (-1 != field.name.search(/\[contents\]\[\d\]\[position\]/)) ? j : field.value;
+      });
+    total++;
+    };
+
+    $(this).sortable($.extend({receive:this.receive}, $.composition.sortableOptions));
+  });
+}
+
+$.fn.designelement = function(canvas)
+{
+  return this.draggable({connectToSortable:$(canvas).find('.content > .positions_container'), helper: 'clone'});
+}
+
+$.fn.contentelement = function(designelements)
+{
+  return this.draggable({connectToSortable:$(designelements).find('.positions_container'), helper: 'clone'});
+}
+
+// -------------------
+
+$(document).ready(function(){
+  var canvas = $('.sf_admin_form > form').canvas();
+  var designelements = $('#design_element_source_list > li').designelement(canvas);
+  $('#content_element_source_list .contents > li').contentelement(designelements);
+});
+
